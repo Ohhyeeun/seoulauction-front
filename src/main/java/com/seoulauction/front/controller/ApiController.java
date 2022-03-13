4295e186dd1ecef28a6f6bdaa4bdca1c903bc9ac
sjk <sjk@seoulauction.com>
2022-03-13 23:38:18 +0900

    Feat: 신세계 회원탈회 API 적용

--- src/main/java/com/seoulauction/front/controller/ApiController.java
+++ src/main/java/com/seoulauction/front/controller/ApiController.java
@@ -1,13 +1,25 @@
 package com.seoulauction.front.controller;
 
+import com.seoulauction.front.util.AuctionUtil;
 import com.seoulauction.ws.service.CommonService;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import javax.lang.model.element.Element;
+import javax.xml.ws.Response;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,21 +27,37 @@ import java.util.Map;
 @Controller("apiController")
 public class ApiController {
 
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private CommonService commonService;
 
+    //@Autowired
+    //EhCacheCacheManager ehCacheCacheManager;
+
+    //@Autowired
+    //RedisTemplate redisTemplate;
+
     @RequestMapping(value = "/api/lots", method = RequestMethod.GET, produces = "application/json;charset=utf8")
     @ResponseBody
+    //@Cacheable(value="apiLots")
     public JSONObject lots(@RequestParam Integer saleNumber, @RequestParam(value = "lotNumbers") List<Integer> lotNumbers) {
-        System.out.println("/api/lots");
-        JSONObject result = new JSONObject();
-
-        System.out.println("/api/lots saleNumber: "+saleNumber);
+        logger.info("/api/lots");
+        logger.info("/api/lots saleNumber: "+saleNumber);
         for(int lot : lotNumbers) {
-            System.out.println("/api/lots lot: "+lot);
+            logger.info("/api/lots lot: "+lot);
         }
+/*
+        Collections.sort(lotNumbers);
 
-        HashMap<String,Object> paramMap = new HashMap<>();
+        Cache c = ehCacheCacheManager.getCache("apiLots");
+        Object element = c.get(lotNumbers);
+        logger.info("element:" +element);
+
+        redisTemplate.opsForValue().set(lotNumbers, "");
+*/
+
+        Map<String,Object> paramMap = new HashMap<>();
         paramMap.put("saleNumber", saleNumber);
         paramMap.put("lotNumbers", lotNumbers);
 
@@ -60,10 +88,50 @@ public class ApiController {
             lotsData.add(lot);
         }
 
+        JSONObject result = new JSONObject();
         result.put("saleNumber", saleNumber);
         result.put("lotData", lotsData);
 
         return result;
     }
 
+    @RequestMapping(value = "/api/custs/leave/{custId}", method = RequestMethod.POST, produces = "application/json;charset=utf8")
+    @ResponseBody
+    public ResponseEntity<JSONObject> cust_leave(@PathVariable(value = "custId") String custId) {
+        logger.info("/api/custs/leave/"+custId);
+
+        try {
+            JSONObject result = new JSONObject();
+
+            // custId AES256 복호화
+            custId = AuctionUtil.aesDecryptSSG(custId);
+            logger.info("/api/custs/leave/" + custId);
+
+            // check CUST
+            Map<String, Object> paramMap = new HashMap<String, Object>();
+            paramMap.put("login_id", custId);
+            paramMap.put("stat_cd", "normal");
+            Map<String, Object> resultMap = commonService.getData("get_customer_by_login_id", paramMap);
+            if (resultMap == null || resultMap.isEmpty()) {
+                result.put("result", "fail");
+                result.put("message", "NOT_FOUND");
+                return new ResponseEntity<>(result, HttpStatus.NOT_FOUND);
+            }
+
+            // leave CUST
+            paramMap.put("action_user_no", resultMap.get("CUST_NO"));
+            commonService.modifyData("modify_cust_stat_cd_for_leave", paramMap);
+
+            result.put("result", "success");
+            result.put("message", "OK");
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+
+            JSONObject result = new JSONObject();
+            result.put("result", "fail");
+            result.put("message", "INTERNAL_SERVER_ERROR");
+            return new ResponseEntity<>(result, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
