d4ed5587876bb4228eb91e4da0054a2e7a0d1087
sjk <sjk@seoulauction.com>
2022-03-14 22:00:57 +0900

    Feat: 신세계 로그인 방식 변경

---
+++ src/main/java/com/seoulauction/front/auth/SSGAuthenticationProvider.java
@@ -0,0 +1,94 @@
+package com.seoulauction.front.auth;
+
+import com.seoulauction.common.auth.PasswordEncoderAESforSA;
+import com.seoulauction.common.auth.SAUserDetails;
+import com.seoulauction.front.util.AuctionUtil;
+import com.seoulauction.ws.dao.CommonDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SSGAuthenticationProvider  implements AuthenticationProvider {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    PasswordEncoderAESforSA encoder;
+
+    @Autowired
+    private CommonDao commonDao;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        logger.info("SSG authenticate");
+
+        String custId = (String) authentication.getPrincipal();
+        try {
+            // AES256 복호화
+            custId = AuctionUtil.aesDecryptSSG(custId);
+        } catch (Exception e) {
+            logger.error("aesDecryptSSG error");
+            throw new BadCredentialsException("aesDecryptSSG error");
+        }
+
+        logger.info("custId : {}" , custId);
+
+        // check CUST
+        Map<String, Object> paramMap = new HashMap<String, Object>();
+        paramMap.put("login_id", custId);
+        paramMap.put("stat_cd", "normal");
+        Map<String, Object> resultMap = commonDao.selectOne("get_customer_by_login_id", paramMap);
+        System.out.println("preHandle resultMap: " + resultMap);
+        if (resultMap == null || resultMap.isEmpty()) {
+            //insert CUST
+            paramMap.put("local_kind_cd", "korean");
+            paramMap.put("nation_cd", "KR");
+            paramMap.put("join_kind_cd", "online");
+            paramMap.put("cust_name", "SSG");
+            paramMap.put("cust_kind_cd", "person");
+            paramMap.put("login_id", custId);
+            commonDao.insert("add_join", paramMap);
+
+            resultMap = commonDao.selectOne("get_customer_by_login_id", paramMap);
+        }
+
+        // Get the IP address of the user tyring to use the site
+        WebAuthenticationDetails wad = (WebAuthenticationDetails) authentication.getDetails();
+        String userIPAddress = wad.getRemoteAddress();
+
+        paramMap.put("ip", userIPAddress);
+        paramMap.put("user_no", resultMap.get("CUST_NO"));
+        paramMap.put("user_kind_cd", "customer");
+        commonDao.insert("insert_conn_hist", paramMap);
+
+        List<GrantedAuthority> roles = new ArrayList<GrantedAuthority>();
+        roles.add(new SimpleGrantedAuthority("ROLE_FRONT_USER"));
+
+        int custNo = Integer.parseInt(resultMap.get("CUST_NO").toString());
+
+        UsernamePasswordAuthenticationToken result
+                = new UsernamePasswordAuthenticationToken(custNo, resultMap.get("PASSWD").toString(), roles);
+        result.setDetails(new SAUserDetails(custId, resultMap.get("PASSWD").toString(), custNo, roles, resultMap.get("CUST_KIND_CD").toString()));
+
+        return result;
+    }
+
+    @Override
+    public boolean supports(Class<? extends Object> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+}
+
--- src/main/java/com/seoulauction/front/controller/LoginController.java
+++ src/main/java/com/seoulauction/front/controller/LoginController.java
@@ -1,15 +1,28 @@
 package com.seoulauction.front.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import com.seoulauction.front.auth.FrontAuthenticationProvider;
+import com.seoulauction.front.auth.SSGAuthenticationProvider;
+import com.seoulauction.front.util.AuctionUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +33,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class LoginController {
 
 	protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	SSGAuthenticationProvider ssgAuthenticationProvider;
 
 	@RequestMapping(value = {"/login"}, method = RequestMethod.GET)
 	public String login(ModelMap model, HttpServletRequest request,
@@ -62,5 +78,25 @@ public class LoginController {
 		model.addAttribute("targetUrl", targetUrl);
 		return "customer/login";
 
+	}
+
+	@RequestMapping(value = {"/api/login"}, method = RequestMethod.GET)
+	public String ssg_login(HttpServletRequest request, @RequestParam(value = "custId") String custId, @RequestParam(value="callbackUrl", required = false) String callbackUrl) {
+		logger.info("/api/login");
+
+		WebAuthenticationDetails details = new WebAuthenticationDetails(request);
+		SecurityContext sc = SecurityContextHolder.getContext();
+		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(custId, null);
+		auth.setDetails(details);
+		sc.setAuthentication(ssgAuthenticationProvider.authenticate(auth));
+
+		HttpSession session = request.getSession(true);
+		session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, sc);
+
+		if(callbackUrl == null) {
+			return "redirect:/";
+		}
+		logger.info("/api/login {}", callbackUrl);
+		return "redirect:"+callbackUrl;
 	}
 }
